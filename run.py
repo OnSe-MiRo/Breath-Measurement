bb45b7b20f6c2f0ad9f07c21c4eb418312879b6c
OnSe-MiRo <neo7513@naver.com>
2022-10-18 12:00:33 +0900

    run file

--- run.py
+++ run.py
@@ -1,22 +1,24 @@
-from turtle import color
-import mediapipe as mp
 from utils.camera import *
 from utils.BodyPose import *
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import cv2
+import numpy as np
 
 PipeLine=open_camera()
 pose,mp_pose,mp_drawing,mp_drawing_styles=bodypose()
-
+raw_data=[]
 def main(i):
     color_image,depth_image=frame(pipeline=PipeLine)
     pose_landmarks=result_bodypose(pose,mp_pose,mp_drawing,mp_drawing_styles,color_image,depth_image)
+
+    if pose_landmarks!=None:
+        BodyPoints=body_points(pose_landmarks,color_image.shape)
+        if BodyPoints!=None:
+            Breath_roi_image=breath_roi(BodyPoints,depth_image)
+            raw_data.append(np.mean(Breath_roi_image))
     try:
-        x=[1,2,3,4]
-        y=[6,7,8,9]
         plt.cla()
-        plt.plot(x,y,label='depth')
+        plt.plot(raw_data,label='depth')
         plt.legend()
     except:
         pass
