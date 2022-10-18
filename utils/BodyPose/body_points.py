be4299fbf9650751a2c354ba3e16e455cd49172a
OnSe-MiRo <neo7513@naver.com>
2022-10-18 12:00:23 +0900

    body points

--- utils/BodyPose/body_points.py
+++ utils/BodyPose/body_points.py
@@ -7,10 +7,13 @@ def return_point(pose_data,shape):
     return (x,y)
 
 def body_points(pose_landmarks,color_colormap_dim):
-    assert len(pose_landmarks.landmark)==3,  'Unexpected number of predicted pose landmarks: {}'.format(len(pose_landmarks.landmark))
-    pose_landmarks = [[lmk.x, lmk.y, lmk.z] for lmk in pose_landmarks.landmark]
-    Right_Shoulder=return_point(pose_landmarks[12],color_colormap_dim)
-    LEft_Shoulder=return_point(pose_landmarks[11],color_colormap_dim)
-    Right_Waist=return_point(pose_landmarks[24],color_colormap_dim)
-    Left_Waist=return_point(pose_landmarks[23],color_colormap_dim)
-    return [Right_Shoulder,LEft_Shoulder,Right_Waist,Left_Waist]
+    try:
+        assert len(pose_landmarks.landmark)==33,  'Unexpected number of predicted pose landmarks: {}'.format(len(pose_landmarks.landmark))
+        pose_landmarks = [[lmk.x, lmk.y, lmk.z] for lmk in pose_landmarks.landmark]
+        Right_Shoulder=return_point(pose_landmarks[12],color_colormap_dim)
+        LEft_Shoulder=return_point(pose_landmarks[11],color_colormap_dim)
+        Right_Waist=return_point(pose_landmarks[24],color_colormap_dim)
+        Left_Waist=return_point(pose_landmarks[23],color_colormap_dim)
+        return [Right_Shoulder,LEft_Shoulder,Right_Waist,Left_Waist]
+    except:
+        return None
