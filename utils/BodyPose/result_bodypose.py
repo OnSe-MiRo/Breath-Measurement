81bec93e35193d6ee843da6dad5c4ea4ecca47bb
OnSeMiRo <79685152+OnSe-MiRo@users.noreply.github.com>
2022-10-26 15:47:24 +0900

    Update result_bodypose.py

--- utils/BodyPose/result_bodypose.py
+++ utils/BodyPose/result_bodypose.py
@@ -6,7 +6,7 @@ def result_bodypose(pose,mp_pose,mp_drawing,mp_drawing_styles,color_image,depth_
     depth_colormap_dim = depth_colormap.shape
     color_colormap_dim = color_image.shape
     pose_results=pose.process(color_image)
-    
+     
     if depth_colormap_dim != color_colormap_dim:
         resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
         images = np.hstack((resized_color_image, depth_colormap))
