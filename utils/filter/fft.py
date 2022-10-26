8a3647392d4e2b80d13c231c8c12ce8257faaed0
OnSeMiRo <79685152+OnSe-MiRo@users.noreply.github.com>
2022-10-26 15:44:52 +0900

    Update fft.py

--- utils/filter/fft.py
+++ utils/filter/fft.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-def fft(data):
+def fft(data,time_data):
     n=len(data)
     k=np.arange(n)
-    Fs=60
-    T=n/Fs
+    T=abs(time_data[0]-time_data[-1])
     freq=k/T
     freq=freq[range(int(n/2))]
     Y=np.fft.fft(data)/n
-    print(data)
     Y=Y[range(int(n/2))]
+    
     return Y,freq
