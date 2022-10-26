06db6f89d44a39e3603db3a493ba98c6105802cd
OnSeMiRo <79685152+OnSe-MiRo@users.noreply.github.com>
2022-10-26 15:50:26 +0900

    Update run.py

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 import time
 def main(i):
-    time1=time.time()
+    t=time.time()
     global raw_data
+    global time_data
+    
     color_image,depth_image=frame(pipeline=PipeLine)
     pose_landmarks=result_bodypose(pose,mp_pose,mp_drawing,mp_drawing_styles,color_image,depth_image)
     XAxisRange=50
@@ -19,27 +21,30 @@ def main(i):
         else: 
             if raw_data!=[]:
                 raw_data.append(raw_data[-1])
+    time_data.append(t)
+    time_data=time_data[-300:]
     
     if len(raw_data) < XAxisRange:  
-        try:
-            plt.cla()
-            plt.plot(raw_data,label='depth')
-            plt.legend()
-        except:
-            pass
+        pass
     else:
         raw_data=raw_data[-500:]
-        smooth_data=smooth_filter.smooth(raw_data,40)
-        plt.cla()
-        plt.plot(raw_data[-200:],label='depth')
-        plt.plot(smooth_data[-240:-40],label='filter')
-        plt.legend()
-    time2=time.time()
-    print(time2-time1)
+        smooth_data=smooth_filter.smooth(raw_data,10)
+        smooth_data_fft_y,freq=fft(smooth_data[-140:-40],time_data[-140:-40])
+        
+        smooth_data_fft_y[0]=0
+        ax1.cla()
+        ax2.cla()
+        ax1.plot(raw_data[-100:],label='depth')
+        ax1.plot(smooth_data[-100:-40],label='filter')
+        ax2.plot(freq,abs(smooth_data_fft_y),label='FFT')
+        
+fig=plt.figure(1)
+ax1=fig.add_subplot(2,1,1)
+ax2=fig.add_subplot(2,1,2)
 error_code=0
 pose,mp_pose,mp_drawing,mp_drawing_styles=bodypose()
 raw_data=[]
-
+time_data=[]
 try:
     PipeLine=open_camera()
     print('INFO: Device Connected')
@@ -50,7 +55,3 @@ except:
 if error_code==0:
     ani = FuncAnimation(plt.gcf(),main, interval = 1000/60)
     plt.show()
-
-
-
-
